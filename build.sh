d8219ca5dbecf6de5a65c4ce1a9f43f0ac4d466b
Daisuke Sato <daisukes@cmu.edu>
2023-04-21 12:31:33 -0400

    add query service
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- build.sh
+++ build.sh
@@ -6,12 +6,44 @@ scriptdir=`dirname $0`
 cd $scriptdir
 scriptdir=`pwd`
 
+skip_mvn_build=0
+with_query=0
 
-pushd $scriptdir/mvn-build
-./build-ear.sh
-popd
+function help() {
+    echo "Usage:"
+    echo " this script build EAR and liberty container"
+    echo ""
+    echo "-h          show this help"
+    echo "-s          skip maven build"
+    echo "-q          build EAR with QueryService (in adittion to MapService and ConversationServiceWatson)"
+}
 
-cp ./mvn-build/home/NavCogConvEAR/target/navcog-conv.ear liberty-build/
+while getopts "sq" arg; do
+    case $arg in
+	s)
+	    skip_mvn_build=1
+	    ;;
+	q)
+	    with_query=1
+	    ;;
+	h)
+	    help
+	    ;;
+    esac
+done
+
+if [[ $skip_mvn_build -eq 0 ]]; then
+    pushd $scriptdir/mvn-build
+    ./build-ear.sh
+    popd
+fi
+
+if [[ $with_query -eq 0 ]]; then
+    cp ./mvn-build/home/NavCogConvEAR/target/navcog-conv.ear liberty-build/
+else
+    cp ./mvn-build/home/NavCogConvQueryEAR/target/navcog-conv.ear liberty-build/
+fi
+
 pushd $scriptdir/liberty-build
 docker-compose build
 popd
--- mvn-build/home/build.sh
+++ mvn-build/home/build.sh
@@ -8,11 +8,27 @@ pushd MapService/MapService
 mvn package
 mvn install
 popd
+
+
 pushd ConversationServiceWatson/ConversationServiceWatson
 mvn package
 mvn install
 popd
+
+
+pushd QueryService
+sh download-lib.sh
+popd
+pushd QueryService/QueryService
+mvn package
+mvn install
+popd
+
+
 pushd NavCogConvEAR
+mvn package
+popd
+pushd NavCogConvQueryEAR
 mvn package
 popd
 
